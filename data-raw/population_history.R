d2e3cea48874c27ba00476c52cfb4e200e61c97a
Michael Lydeamore <michael.lydeamore@monash.edu>
2025-03-25 05:01:40 +0000

    add age/sex data files

--- data-raw/population_history.R
+++ data-raw/population_history.R
@@ -10,19 +10,20 @@ world_id <- get_id("World", type = "locations")
 
 age_id <- get_id("Population by 5-year age groups and sex", type = "Indicators", search = FALSE, .progress = FALSE)
 
-#population_age_sex_fiveyr <- get_indicator_data(indicator_id = age_id$id, location_id = world_id$id, start_year = 1950, end_year = 2024)
+# population_age_sex_fiveyr <- get_indicator_data(indicator_id = age_id$id, location_id = world_id$id, start_year = 1950, end_year = 2024)
 
 all_countries <- get_id("", type = "locations") |>
-  dplyr::filter(!name %in% "Holy See")
+  dplyr::filter(!name %in% "Holy See") |>
+  filter(!is.na(longitude))
 
 ### WARNING
 ### This chunk takes upwards of five hours to run
 ### Use with extreme caution
-
+ii <- 0
 all_country_age_sex_fiveyr <- lapply(cli::cli_progress_along(1:nrow(all_countries)), function(i) {
   if (file.exists(paste0("data-raw/age_sex_fiveyr/age_sex_fiveyr_", all_countries$name[i], ".csv")))
     return (NA)
-  
+    ii <<- i
   population_age_sex_fiveyr <- get_age_data(location_id = all_countries$id[i], age_bracket = 5, by_sex = TRUE, start_year = 1950, end_year = 2023) 
   readr::write_csv(
     x = population_age_sex_fiveyr,
