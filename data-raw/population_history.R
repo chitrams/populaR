d7f7225973c39cb757adf57de1de02fdf5311585
Michael Lydeamore <michael.lydeamore@monash.edu>
2025-03-25 22:00:23 +1100

    Remove global indicator variable for downloading data

--- data-raw/population_history.R
+++ data-raw/population_history.R
@@ -19,11 +19,10 @@ all_countries <- get_id("", type = "locations") |>
 ### WARNING
 ### This chunk takes upwards of five hours to run
 ### Use with extreme caution
-ii <- 0
 all_country_age_sex_fiveyr <- lapply(cli::cli_progress_along(1:nrow(all_countries)), function(i) {
   if (file.exists(paste0("data-raw/age_sex_fiveyr/age_sex_fiveyr_", all_countries$name[i], ".csv")))
     return (NA)
-    ii <<- i
+
   population_age_sex_fiveyr <- get_age_data(location_id = all_countries$id[i], age_bracket = 5, by_sex = TRUE, start_year = 1950, end_year = 2023) 
   readr::write_csv(
     x = population_age_sex_fiveyr,
