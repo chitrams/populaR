941fd161c1cc9260eaf5f44f1fec93b30ec65d87
James Goldie <me@jamesgoldie.dev>
2025-03-31 16:15:41 +1100

    Set a dummy API key when using vcr for testing

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,4 +1,7 @@
 library("vcr") # *Required* as vcr is set up on loading
+if (!nzchar(Sys.getenv("WPP_API_KEY"))) {
+  Sys.setenv("WPP_API_KEY" = "foobar")
+}
 invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures")
 ))
